1c463d177bfac870e82957e97864680d33477bf6
Richard Haythornthwaite <richardhaythornthwaite@hotmail.co.uk>
2021-02-23 16:42:53 +0000

    trying least squares percentage error

--- alongtrackwinds.py
+++ alongtrackwinds.py
@@ -21,10 +21,14 @@ from lmfit.models import GaussianModel
 from lmfit import Parameters
 from lmfit import Minimizer
 
+from symfit.core.objectives import HessianObjective
+from symfit.core.minimizers import LBFGSB
+from symfit.core.support import keywordonly
 from symfit import parameters, variables, Fit, Model, Parameter, exp
 from symfit.distributions import Gaussian
 
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_absolute_percentage_error
 
 import datetime
 from util import *
@@ -124,6 +128,104 @@ def ELS_maxflux_anode(elsdata, starttime, endtime):
     return maxflux_anode
 
 
+class LeastSquaresPercentageObjective(HessianObjective):
+    @keywordonly(flatten_components=True)
+    def __call__(self, ordered_parameters=[], **parameters):
+        """
+        :param ordered_parameters: See ``parameters``.
+        :param parameters: values of the
+            :class:`~symfit.core.argument.Parameter`'s to evaluate :math:`S` at.
+        :param flatten_components: if `True`, return the total :math:`S`. If
+            `False`, return the :math:`S` per component of the
+            :class:`~symfit.core.models.BaseModel`.
+        :return: scalar or list of scalars depending on the value of `flatten_components`.
+        """
+        flatten_components = parameters.pop('flatten_components')
+        evaluated_func = super(LeastSquaresPercentageObjective, self).__call__(
+            ordered_parameters, **parameters
+        )
+
+        chi2 = [0 for _ in evaluated_func]
+        for index, (dep_var, dep_var_value) in enumerate(zip(self.model.dependent_vars, evaluated_func)):
+            dep_data = self.dependent_data.get(dep_var, None)
+
+            if dep_data is not None:
+
+                sigma = self.sigma_data[self.model.sigmas[dep_var]]
+                #print((dep_var_value - dep_data), (dep_var_value - dep_data) / dep_data)
+                chi2[index] += np.sum(abs((dep_var_value - dep_data) / dep_data) / abs(sigma))
+                #chi2[index] += np.sum((dep_var_value - dep_data) ** 2 / sigma ** 2)
+                #if len(dep_var_value) == 17:
+                    # print("normal",((dep_var_value - dep_data) ** 2 / sigma ** 2))
+                    # print("Percent",((dep_var_value - dep_data) / dep_data) ** 2 / sigma ** 2)
+        chi2 = np.sum(chi2) if flatten_components else chi2
+        return chi2 / 2
+
+    def eval_jacobian(self, ordered_parameters=[], **parameters):
+        """
+        Jacobian of :math:`S` in the
+        :class:`~symfit.core.argument.Parameter`'s (:math:`\\nabla_\\vec{p} S`).
+
+        :param parameters: values of the
+            :class:`~symfit.core.argument.Parameter`'s to evaluate :math:`\\nabla_\\vec{p} S` at.
+        :return: ``np.array`` of length equal to the number of parameters..
+        """
+        evaluated_func = super(LeastSquaresPercentageObjective, self).__call__(
+            ordered_parameters, **parameters
+        )
+        evaluated_jac = super(LeastSquaresPercentageObjective, self).eval_jacobian(
+            ordered_parameters, **parameters
+        )
+
+        result = 0
+        for var, f, jac_comp in zip(self.model.dependent_vars, evaluated_func,
+                                    evaluated_jac):
+            y = self.dependent_data.get(var, None)
+            sigma_var = self.model.sigmas[var]
+            if y is not None:
+                sigma = self.sigma_data[sigma_var]
+                pre_sum = jac_comp * ((y - f) / sigma ** 2)[np.newaxis, ...]
+                axes = tuple(range(1, len(pre_sum.shape)))
+                result -= np.sum(pre_sum, axis=axes, keepdims=False)
+        return np.atleast_1d(np.squeeze(np.array(result)))
+
+    def eval_hessian(self, ordered_parameters=[], **parameters):
+        """
+        Hessian of :math:`S` in the
+        :class:`~symfit.core.argument.Parameter`'s (:math:`\\nabla_\\vec{p}^2 S`).
+
+        :param parameters: values of the
+            :class:`~symfit.core.argument.Parameter`'s to evaluate :math:`\\nabla_\\vec{p} S` at.
+        :return: ``np.array`` of length equal to the number of parameters..
+        """
+        evaluated_func = super(LeastSquaresPercentageObjective, self).__call__(
+            ordered_parameters, **parameters
+        )
+        evaluated_jac = super(LeastSquaresPercentageObjective, self).eval_jacobian(
+            ordered_parameters, **parameters
+        )
+        evaluated_hess = super(LeastSquaresPercentageObjective, self).eval_hessian(
+            ordered_parameters, **parameters
+        )
+
+        result = 0
+        for var, f, jac_comp, hess_comp in zip(self.model.dependent_vars,
+                                               evaluated_func, evaluated_jac,
+                                               evaluated_hess):
+            y = self.dependent_data.get(var, None)
+            sigma_var = self.model.sigmas[var]
+            if y is not None:
+                sigma = self.sigma_data[sigma_var]
+                p1 = hess_comp * ((y - f) / sigma ** 2)[np.newaxis, np.newaxis, ...]
+                # Outer product
+                p2 = np.einsum('i...,j...->ij...', jac_comp, jac_comp)
+                p2 = p2 / sigma[np.newaxis, np.newaxis, ...] ** 2
+                # We sum away everything except the matrices in the axes 0 & 1.
+                axes = tuple(range(2, len(p2.shape)))
+                result += np.sum(p2 - p1, axis=axes, keepdims=False)
+        return np.atleast_2d(np.squeeze(np.array(result)))
+
+
 def IBS_ELS_gaussian(ibs_x, ibs_dataslice, els_x, els_dataslice, cassini_speed, lpvalue, temperature):
     x_1, x_2, y_1, y_2 = variables('x_1, x_2, y_1, y_2')
 
@@ -141,15 +243,15 @@ def IBS_ELS_gaussian(ibs_x, ibs_dataslice, els_x, els_dataslice, cassini_speed, 
                         value=(8 * scipy.constants.physical_constants['Boltzmann constant'][0] * temperature) /
                               scipy.constants.physical_constants['atomic unit of charge'][0])
     temp_eV.fixed = True
-    lp_pot = Parameter("lp_pot", value=lpvalue-0.5, max=lpvalue, min=-2.5)
+    lp_pot = Parameter("lp_pot", value=lpvalue - 0.5, max=lpvalue, min=-2.5)
 
-    ionvelocity = Parameter("ionvelocity", value=-400)
+    ionvelocity = Parameter("ionvelocity", value=-347)
 
     # Negative Ion Parameters
-    mass26_neg_amp = Parameter("mass26_neg_amp", value=1, min=0.1, max=1.5)
-    mass50_neg_amp = Parameter("mass50_neg_amp", value=1, min=0.1, max=1.5)
-    mass74_neg_amp = Parameter("mass74_neg_amp", value=1, min=0.1, max=1.5)
-    mass117_neg_amp = Parameter("mass117_neg_amp", value=1, min=0.1, max=1.5)
+    mass26_neg_amp = Parameter("mass26_neg_amp", value=0.5)
+    mass50_neg_amp = Parameter("mass50_neg_amp", value=0.5)
+    mass74_neg_amp = Parameter("mass74_neg_amp", value=0.5)
+    mass117_neg_amp = Parameter("mass117_neg_amp", value=0.5)
 
     mass26_neg_sig = Parameter("mass26_neg_sig", value=0.3, min=0.2, max=0.7)
     mass50_neg_sig = Parameter("mass50_neg_sig", value=0.62, min=0.4, max=1)
@@ -157,12 +259,12 @@ def IBS_ELS_gaussian(ibs_x, ibs_dataslice, els_x, els_dataslice, cassini_speed, 
     mass117_neg_sig = Parameter("mass117_neg_sig", value=1.5, min=0.8, max=1.8)
 
     # Positive Ion Parameters
-    mass28_amp = Parameter("mass28_amp", value=1, max=1.5)
-    mass41_amp = Parameter("mass41_amp", value=1, max=1.5)
-    mass53_amp = Parameter("mass53_amp", value=1, max=1.5)
-    mass66_amp = Parameter("mass66_amp", value=1, max=1.5)
-    mass78_amp = Parameter("mass78_amp", value=1, max=1.5)
-    mass91_amp = Parameter("mass91_amp", value=1, max=1.5)
+    mass28_amp = Parameter("mass28_amp", value=0.5)
+    mass41_amp = Parameter("mass41_amp", value=0.5)
+    mass53_amp = Parameter("mass53_amp", value=0.5)
+    mass66_amp = Parameter("mass66_amp", value=0.5)
+    mass78_amp = Parameter("mass78_amp", value=0.5)
+    mass91_amp = Parameter("mass91_amp", value=0.5)
 
     mass28_sig = Parameter("mass28_sig", value=0.4, min=0.2, max=0.6)
     mass41_sig = Parameter("mass41_sig", value=0.5, min=0.2, max=0.6)
@@ -205,9 +307,11 @@ def IBS_ELS_gaussian(ibs_x, ibs_dataslice, els_x, els_dataslice, cassini_speed, 
     })
 
     # init_y  = model(x_1=els_x, x_2=ibs_x, **fit_result.params)
+    # print(els_dataslice, ibs_dataslice)
+    minimize_options = {"options":{"disp": True}}
+    fit = Fit(model, x_1=els_x, x_2=ibs_x, y_1=els_dataslice, y_2=ibs_dataslice, objective=LeastSquaresPercentageObjective)
 
-    fit = Fit(model, x_1=els_x, x_2=ibs_x, y_1=els_dataslice, y_2=ibs_dataslice)
-    fit_result = fit.execute()
+    fit_result = fit.execute(**minimize_options)
     print(fit_result)
     tend = time.time()
     print("Fit Run Time", tend - tstart)
@@ -266,7 +370,8 @@ def ELS_IBS_fluxfitting(elsdata, ibsdata, tempdatetime, titanaltitude, ibs_masse
     windspeed = 0
     temperature = titan_linearfit_temperature(titanaltitude)
 
-    ibs_dataslice = ibsdata['ibsdata'][ibs_lowerenergyslice:ibs_upperenergyslice, 1, ibs_slicenumber]
+    ibs_dataslice = np.array(ibsdata['ibsdata'][ibs_lowerenergyslice:ibs_upperenergyslice, 1, ibs_slicenumber])
+    ibs_dataslice[ibs_dataslice <= 0] = 1
     ibs_x = ibscalib['ibsearray'][ibs_lowerenergyslice:ibs_upperenergyslice]
     scaled_ibs_dataslice = ibs_dataslice / max(ibs_dataslice)
 
@@ -275,29 +380,33 @@ def ELS_IBS_fluxfitting(elsdata, ibsdata, tempdatetime, titanaltitude, ibs_masse
     print("anode", anode)
     els_dataslice = np.float32(ELS_backgroundremoval(elsdata, els_slicenumber, els_slicenumber + 1, datatype="data")[
                                els_lowerenergyslice:els_upperenergyslice, anode, 0])
+    els_dataslice[els_dataslice <= 0] = 1
     print(elsdata['flyby'], "Cassini velocity", cassini_speed, "Altitude", titanaltitude)
     els_x = elscalib['earray'][els_lowerenergyslice:els_upperenergyslice]
     scaled_els_dataslice = els_dataslice / max(els_dataslice)
 
-    fit_result, y, ionvelocity, lp_pot = IBS_ELS_gaussian(ibs_x, scaled_ibs_dataslice, els_x, scaled_els_dataslice, cassini_speed, lpvalue,
-                                     temperature)
+    fit_result, y, ionvelocity, lp_pot = IBS_ELS_gaussian(ibs_x, scaled_ibs_dataslice, els_x, scaled_els_dataslice, cassini_speed,
+                                                          lpvalue,
+                                                          temperature)
 
-    stepplotfig, (elsax, ibsax) = plt.subplots(2, sharex='all', sharey='all')
+    stepplotfig, (elsax, ibsax) = plt.subplots(2, sharex='all')
     elsax.plot(els_x, y[0], color='r', label="ELS fit")
     elsax.step(els_x, scaled_els_dataslice, where='mid', color='k', label="ELS data")
-    elsax.set_ylim(0.01, 1.25)
+    elsax.set_ylim(min(scaled_els_dataslice), max(scaled_els_dataslice) * 1.5)
     elsax.set_yscale("log")
     elsax.set_ylabel("Normalised to max flux bin \n in reduced energy range", fontsize=15)
     elsax.set_title("ELS data from " + elsdata['flyby'] + " " + str(tempdatetime), fontsize=24)
     elsax.legend()
-    elsax.text(10, 0.015,"Ion velocity = %2.2f ± %2.2f m/s" % (fit_result.value(ionvelocity),
-               fit_result.stdev(ionvelocity)))
+    elsax.text(10, 0.015, "Ion velocity = %2.2f ± %2.2f m/s" % (fit_result.value(ionvelocity),
+                                                                fit_result.stdev(ionvelocity)))
     elsax.text(10, 0.02, "CAPS S/C potential = %2.2f ± %2.2f V" % (
-    fit_result.value(lp_pot), fit_result.stdev(lp_pot)))
+        fit_result.value(lp_pot), fit_result.stdev(lp_pot)))
     elsax.text(10, 0.025, "LP S/C potential = %2.2f V" % lpvalue)
 
     ibsax.plot(ibs_x, y[1], color='r', label="IBS fit")
     ibsax.step(ibs_x, scaled_ibs_dataslice, where='mid', color='k', label="IBS data")
+    ibsax.set_yscale("log")
+    ibsax.set_ylim(min(scaled_ibs_dataslice), max(scaled_ibs_dataslice) * 1.5)
     ibsax.set_xlabel("Energy (ev/q)", fontsize=20)
     ibsax.set_ylabel("Normalised to max flux bin \n in reduced energy range", fontsize=15)
     ibsax.set_title("IBS data from " + elsdata['flyby'] + " " + str(tempdatetime), fontsize=24)
@@ -323,7 +432,7 @@ def multiple_alongtrackwinds_flybys(usedflybys):
         generate_aligned_ibsdata(ibsdata, elsdata, flyby)
         for (i, j) in zip(tempdf['Positive Peak Time'], tempdf['Altitude']):
             fit_result, lpvalue, ionwind_param, scp_pot_param, cassini_speed = ELS_IBS_fluxfitting(elsdata, ibsdata,
-                                                               i,j)
+                                                                                                   i, j)
             fit_results.append(fit_result)
             ionwindspeeds.append(fit_result.value(ionwind_param))
             ionwindspeeds_errors.append(fit_result.stdev(ionwind_param))
@@ -339,7 +448,7 @@ def multiple_alongtrackwinds_flybys(usedflybys):
     outputdf['CAPS spacecraft potentials'] = scps
     outputdf['Actual spacecraft velocity'] = cassini_speeds
     outputdf.to_csv("testalongtrackvelocity.csv")
-    #print(outputdf['Positive Peak Time'])
+    # print(outputdf['Positive Peak Time'])
 
     fig5, axes = plt.subplots(2)
     axes[0].errorbar(tempdf['Positive Peak Time'], ionwindspeeds, yerr=np.array(ionwindspeeds_errors),
@@ -371,7 +480,7 @@ def single_slice_test(flyby, slicenumber):
                                  tempdf['Altitude'].iloc[slicenumber])
 
 
-#multiple_alongtrackwinds_flybys(['t16'])
-single_slice_test(flyby="t16", slicenumber=4)
+# multiple_alongtrackwinds_flybys(['t16'])
+single_slice_test(flyby="t16", slicenumber=6)
 
 plt.show()
