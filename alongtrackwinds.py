e3b301a43f331207af3f68b8b74c006f21c719b3
Richard Haythornthwaite <richardhaythornthwaite@hotmail.co.uk>
2021-02-26 14:14:01 +0000

    changing to background removed dataslice

--- alongtrackwinds.py
+++ alongtrackwinds.py
@@ -55,24 +55,32 @@ filedates = {"t16": "22-jul-2006", "t17": "07-sep-2006",
              "t30": "12-may-2007", "t32": "13-jun-2007",
              "t42": "25-mar-2008", "t46": "03-nov-2008", "t47": "19-nov-2008"}
 
-IBS_fluxfitting_dict = {"mass28_": {"sigma": 0.4, "amplitude": []},
-                        "mass40_": {"sigma": 0.5, "amplitude": []},
-                        "mass53_": {"sigma": 0.5, "amplitude": []},
-                        "mass66_": {"sigma": 0.6, "amplitude": []}, \
-                        "mass78_": {"sigma": 0.7, "amplitude": []}, \
-                        "mass91_": {"sigma": 0.8, "amplitude": []}}
+IBS_fluxfitting_dict = {"mass28_": {"sigma": [0.3,0.4,0.6], "amplitude": []},
+                        "mass40_": {"sigma": [0.3,0.6,0.7], "amplitude": []},
+                        "mass53_": {"sigma": [0.3,0.5,0.6],"amplitude": []},
+                        "mass66_": {"sigma": [0.4,0.6,0.7], "amplitude": []}, \
+                        "mass78_": {"sigma": [0.5,0.7,0.8], "amplitude": []}, \
+                        "mass91_": {"sigma": [0.6,0.8,0.9], "amplitude": []}}
 
-ELS_fluxfitting_dict = {"mass26_": {"sigma": 0.3, "amplitude": [5]},
-                        "mass50_": {"sigma": 0.4, "amplitude": [4]},
-                        "mass74_": {"sigma": 0.5, "amplitude": [3]},
-                        "mass117_": {"sigma": 0.7, "amplitude": [3]}}
+ELS_fluxfitting_dict = {"mass26_": {"sigma": [0.2,0.4,0.5], "amplitude": [5]},
+                        "mass50_": {"sigma": [0.3,0.5,0.7], "amplitude": [4]},
+                        "mass74_": {"sigma": [0.5,0.7,1.2], "amplitude": [3]},
+                        "mass117_": {"sigma": [0.5,0.7,1.6], "amplitude": [3]}}
 
-IBS_energybound_dict = {"t16": [4, 17.1], "t17": [3.5, 16.25],
+IBS_energybound_dict = {"t16": [4, 17], "t17": [3.5, 16.25],
                         "t20": [3.5, 16.5], "t21": [4.25, 16.75], "t25": [4.25, 18.25], "t26": [4.35, 18.25],
                         "t27": [4.5, 18.25],
                         "t28": [4.5, 18.25], "t29": [4.5, 18.25],
-                        "t30": [3.5, 16.25], "t32": [3.5, 16.25],
-                        "t42": [3.5, 16.25], "t46": [3.5, 16.25], "t47": [3.5, 16.25]}
+                        "t30": [4.5, 18.25], "t32": [4.5, 18.25],
+                        "t42": [4.5, 19.5], "t46": [3.75, 17.5], "t47": [4.5, 18.25]}
+
+ELS_energybound_dict = {"t16": [1, 30], "t17": [1, 35],
+                        "t20": [1, 35], "t21": [1, 35], "t25": [1, 35], "t26": [1, 35],
+                        "t27": [1, 30],
+                        "t28": [1, 35], "t29": [1, 35],
+                        "t30": [1, 35], "t32": [1, 35],
+                        "t42": [1, 35], "t46": [1, 35], "t47": [1, 35]}
+
 
 
 def energy2mass(energyarray, spacecraftvelocity, ionvelocity, spacecraftpotential, iontemperature=150, charge=1):
@@ -92,10 +100,10 @@ def total_fluxgaussian(xvalues, yvalues, masses, cassini_speed, windspeed, LPval
     pars = Parameters()
     eval_pars = Parameters()
 
-    pars.add('scp', value=LPvalue, min=LPvalue-0.5, max=LPvalue + 0.5)
+    pars.add('scp', value=LPvalue, min=LPvalue-1, max=LPvalue + 0.5)
     pars.add('temp_eV', value=8*k*temperature)  # , min=130, max=170)
     pars.add('spacecraftvelocity', value=cassini_speed)
-    pars.add('ionvelocity', value=0, min=-600, max=600)
+    pars.add('ionvelocity', value=0, min=-400, max=400)
     #pars['scp'].vary = False
     pars['spacecraftvelocity'].vary = False
     pars['temp_eV'].vary = False
@@ -112,11 +120,11 @@ def total_fluxgaussian(xvalues, yvalues, masses, cassini_speed, windspeed, LPval
         gaussmodels.append(GaussianModel(prefix=tempprefix))
         pars.add(tempprefix, value=mass, vary=False)
         if charge == 1:
-            sigmaval = IBS_fluxfitting_dict[tempprefix]['sigma']
+            sigmavals = IBS_fluxfitting_dict[tempprefix]['sigma']
             #ampval = IBS_fluxfitting_dict[tempprefix]['amplitude'][0]
         elif charge == -1:
             # sigmaval = ELS_fluxfitting_dict[tempprefix]['sigma']
-            sigmaval = 0.1 * (mass / 5.64)
+            sigmavals = ELS_fluxfitting_dict[tempprefix]['sigma']
             ampval = ELS_fluxfitting_dict[tempprefix]['amplitude'][0]
 
         # effectivescpexpr = 'scp + ((' + tempprefix + '*AMU*spacecraftvelocity)/e)*windspeed' #Windspeed defined positive if going in same direction as Cassini
@@ -131,7 +139,7 @@ def total_fluxgaussian(xvalues, yvalues, masses, cassini_speed, windspeed, LPval
             value=peakflux(mass, cassini_speed, 0, LPvalue + 0.25, temperature, charge=charge), expr=peakfluxexpr)
         # min=temppeakflux - 2, max=temppeakflux + 2)
 
-        pars[tempprefix + 'sigma'].set(value=sigmaval, min=0.5 * sigmaval, max=2 * sigmaval)
+        pars[tempprefix + 'sigma'].set(value=sigmavals[1], min=sigmavals[0], max=sigmavals[2])
         pars[tempprefix + 'amplitude'].set(value=np.mean(yvalues) * (1 + (0.1 * masscounter)), min=min(yvalues))
 
     for counter, model in enumerate(gaussmodels):
@@ -241,21 +249,21 @@ def ELS_maxflux_anode(elsdata, starttime, endtime):
     return maxflux_anode
 
 def ELS_fluxfitting(elsdata, tempdatetime, titanaltitude, anode=4, lpvalue=-1.3, els_masses = [26, 50, 74, 117]):
-    slicenumber = CAPS_slicenumber(elsdata, tempdatetime)
+    els_slicenumber = CAPS_slicenumber(elsdata, tempdatetime)
 
     temputc = str(titan_flybydates[elsdata['flyby']][0]) + '-' + str(titan_flybydates[elsdata['flyby']][1]) + '-' + str(
-        titan_flybydates[elsdata['flyby']][2]) + 'T' + elsdata['times_utc_strings'][slicenumber]
+        titan_flybydates[elsdata['flyby']][2]) + 'T' + elsdata['times_utc_strings'][els_slicenumber]
     tempphase = cassini_phase(temputc)
     cassini_speed = np.sqrt((tempphase[3]) ** 2 + (tempphase[4]) ** 2 + (tempphase[5]) ** 2) * 1e3
 
-    windspeed = -150
+    windspeed = 0
     temperature = titan_linearfit_temperature(titanaltitude)
 
     els_lowerenergyslice = CAPS_energyslice("els", ELS_energybound_dict[elsdata['flyby']][0] - lpvalue,
                                             ELS_energybound_dict[elsdata['flyby']][0] - lpvalue)[0]
     els_upperenergyslice = CAPS_energyslice("els", ELS_energybound_dict[elsdata['flyby']][1] - lpvalue,
                                             ELS_energybound_dict[elsdata['flyby']][1] - lpvalue)[0]
-    x = elscalib['earray']
+    x = elscalib['earray'][els_lowerenergyslice:els_upperenergyslice]
     anode = ELS_maxflux_anode(elsdata, tempdatetime - datetime.timedelta(seconds=10),
                               tempdatetime + datetime.timedelta(seconds=10))
     print("anode", anode)
@@ -276,7 +284,7 @@ def ELS_fluxfitting(elsdata, tempdatetime, titanaltitude, anode=4, lpvalue=-1.3,
     stepplotax_els.grid(b=True, which='minor', color='k', linestyle='--', alpha=0.25)
     stepplotax_els.minorticks_on()
     stepplotax_els.set_title(
-        "Histogram of " + elsdata['flyby'].upper() + " els data from " + elsdata['times_utc_strings'][slicenumber],
+        "Histogram of " + elsdata['flyby'].upper() + " els data from " + elsdata['times_utc_strings'][els_slicenumber],
         fontsize=32)
     # stepplotax.plot(elscalib['earray'],smoothedcounts_full,color='k')
 
@@ -286,11 +294,12 @@ def ELS_fluxfitting(elsdata, tempdatetime, titanaltitude, anode=4, lpvalue=-1.3,
                              charge=-1,
                              FWHM=ELS_FWHM)
 
+    stepplotax_els.plot(x, out.init_fit, 'b-', label='init fit')
     stepplotax_els.plot(x, out.best_fit, 'r-', label='best fit')
     if out.params['ionvelocity'].stderr is None:
-        out.params['ionvelocity'].stderr = out.params['ionvelocity']
+        out.params['ionvelocity'].stderr = abs(out.params['ionvelocity'])
     if out.params['scp'].stderr is None:
-        out.params['scp'].stderr = out.params['scp']
+        out.params['scp'].stderr = abs(out.params['scp'])
     stepplotax_els.text(0.8, 0.05, "Ion wind = %2.2f ± %2.2f m/s" % (out.params['ionvelocity'], out.params['ionvelocity'].stderr), transform=stepplotax_els.transAxes)
     stepplotax_els.text(0.8, .07, "SC Potential = %2.2f ± %2.2f V" % (out.params['scp'], out.params['scp'].stderr), transform=stepplotax_els.transAxes)
     stepplotax_els.text(0.8, .09, "Temp = %2.2f" % temperature, transform=stepplotax_els.transAxes)
@@ -355,7 +364,7 @@ def single_slice_test(flyby, slicenumber):
     els_ionwindspeed = ELS_fluxfitting(elsdata, tempdf['Negative Peak Time'].iloc[slicenumber],
                                        tempdf['Altitude'].iloc[slicenumber])
 
-single_slice_test("t16",slicenumber=2)
+single_slice_test("t16",slicenumber=3)
 
 
 plt.show()
